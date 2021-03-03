5bbafb3007453e6bf8d70cba8bbd873e486e17ed
Ranjith Thangaraju <63498273+ranjith1010@users.noreply.github.com>
2021-03-03 08:08:06 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,7 @@
+result = {"pykara.jpg":['Rishi', 'Deepak', 'Divakar', 'Sakthi', 'Selvin', 'Tamil', 'Balaji', 'Suresh', 'Mohan', 'Shoba', 'Nantha', 'Basith', 'Jala', 'Suresh', 'Nithesh', 'Abishek', 'Nishanth', 'Naveen', 'Mounish', 'Ranjith', 'Sathya', 'Mukesh'],
+          "indoor.jpg":['Tamil','Mounish','Rishi','Ranjith','Balaji','Nithesh'],
+          "park.jpg":['Divakar','Nithesh','Abishek','Basith','Tamil','Deepak','Rishi','Nishanth'],
+          "park1.jpg":['Nithesh','Mounish','Divakar','Tamil','Basith'],
+          "pine.jpg":['Mukesh','Nithesh','Ranjith','Mounish','Balaji','Rishi','Tamil'],
+          "temple.jpg":['Mounish','Rishi','Ranjith','Divakar','Tamil','Mounish','Mukesh','Nithesh','Balaji','Suresh'],
+          "jalasis.jpg":['Jala','Suresh','Sakthi','Abishek','Shoba','Ranjith','Deepak']}
